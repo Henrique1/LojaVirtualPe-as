6abeca5bc60f4c720bc5d8758ddac8123fe5ef00
Henrique <henrique.balbino.silva@hotmail.com>
2014-11-01 17:17:08 -0200

    Adcionado projeto UI_Web

---
+++ LojaVirtual/LojaVirtual.UI.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LojaVirtual.UI.Web.Startup))]
+namespace LojaVirtual.UI.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
